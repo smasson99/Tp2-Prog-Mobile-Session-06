725146018818525a67c34d679aedd5b65fff22e5
FlyJonat <flyjonat@live.fr>
2019-01-27 22:12:50 -0500

    update

--- app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionActivityViewModel.kt
+++ app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionActivityViewModel.kt
@@ -1,8 +1,16 @@
 package ca.csf.mobile2.tp2.question
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
+import org.parceler.Parcel
 import org.parceler.ParcelConstructor
+import ca.csf.mobile2.tp2.R
+import ca.csf.mobile2.tp2.util.ViewModelProperty
 
+@Parcel(Parcel.Serialization.BEAN)
 class CreateQuestionActivityViewModel @ParcelConstructor constructor() : BaseObservable() {
 
+    @get:Bindable
+    @set:Bindable
+    var questionText:String by ViewModelProperty("",this)
 }
--- app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionService.kt
+++ app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionService.kt
@@ -1,7 +1,57 @@
 package ca.csf.mobile2.tp2.question
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.androidannotations.annotations.Background
 import org.androidannotations.annotations.EBean
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.http.POST
+import java.io.IOException
+
+//todo:bonne url
+//private const val URL="https://m2t2.csfpwmjv.tk/"
 
 @EBean(scope = EBean.Scope.Singleton)
 class CreateQuestionService{
+
+    private val service: Service
+
+    init {
+        val jackson = ObjectMapper()
+            .registerKotlinModule()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(URL)
+            .addConverterFactory(JacksonConverterFactory.create(jackson))
+            .build()
+
+        service = retrofit.create(Service::class.java)
+    }
+
+    //todo:envoyer message réussite
+    @Background
+    fun postQuestion(question:String, onSuccess:()->Unit,
+                     onConnectivityError:()->Unit,
+                     onServerError:()->Unit){
+        try{
+            val response=service.postQuestion(question).execute()
+            if(response.isSuccessful){
+                onSuccess()
+            }
+            else{
+                onServerError()
+            }
+        }
+        catch(e:IOException){
+            onConnectivityError
+        }
+    }
+
+    private interface Service {
+
+        //todo:les paramêtres font quoi?
+        @POST("/api/v1/question/{id}/choose1")
+        fun postQuestion(question:String) : Call<Question>
+    }
 }
