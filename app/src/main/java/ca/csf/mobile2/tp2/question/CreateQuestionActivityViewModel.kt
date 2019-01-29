ab2e6d353292333a97b6a007782b92f899bc3925
FlyJonat <flyjonat@live.fr>
2019-01-29 13:42:31 -0500

    send data

--- app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionActivityViewModel.kt
+++ app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionActivityViewModel.kt
@@ -4,7 +4,6 @@ import android.databinding.BaseObservable
 import android.databinding.Bindable
 import org.parceler.Parcel
 import org.parceler.ParcelConstructor
-import ca.csf.mobile2.tp2.R
 import ca.csf.mobile2.tp2.util.ViewModelProperty
 
 @Parcel(Parcel.Serialization.BEAN)
--- app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionService.kt
+++ app/src/main/java/ca/csf/mobile2/tp2/question/CreateQuestionService.kt
@@ -7,14 +7,15 @@ import org.androidannotations.annotations.EBean
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.POST
 import java.io.IOException
 
 //todo:bonne url
-private const val URL="https://m2t2.csfpwmjv.tk/"
+private const val URL = "https://m2t2.csfpwmjv.tk/"
 
 @EBean(scope = EBean.Scope.Singleton)
-class CreateQuestionService{
+class CreateQuestionService {
 
     private val service: Service
 
@@ -29,29 +30,26 @@ class CreateQuestionService{
         service = retrofit.create(Service::class.java)
     }
 
-    //todo:envoyer message réussite
     @Background
-    fun postQuestion(question:String, onSuccess:()->Unit,
-                     onConnectivityError:()->Unit,
-                     onServerError:()->Unit){
-        try{
-            val response=service.postQuestion(question).execute()
-            if(response.isSuccessful){
+    fun postQuestion(
+        question: Question, onSuccess: () -> Unit,
+        onConnectivityError: () -> Unit,
+        onServerError: () -> Unit
+    ) {
+        try {
+            val response = service.postQuestion(question).execute()
+            if (response.isSuccessful) {
                 onSuccess()
-            }
-            else{
+            } else {
                 onServerError()
             }
-        }
-        catch(e:IOException){
-            onConnectivityError
+        } catch (e: IOException) {
+            onConnectivityError()
         }
     }
 
     private interface Service {
-
-        //todo:les paramêtres font quoi?
-        @POST("/api/v1/question/{id}/choose1")
-        fun postQuestion(question:String) : Call<Question>
+        @POST("/api/v1/question")
+        fun postQuestion(@Body question: Question): Call<Question>
     }
 }
